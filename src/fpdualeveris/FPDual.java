146c7bb58887ef551da8170c7b66d55c2a528b61
fprietoa <fprietoa@everis.com>
2021-05-14 13:45:19 +0200

    EverisJavaDesafio1FPA v2

--- src/fpdualeveris/FPDual.java
+++ src/fpdualeveris/FPDual.java
@@ -2,15 +2,29 @@ package fpdualeveris;
 
 import variables.Developer;
 
+/**
+ * Formación Java
+ * 
+ * @author fprietoa
+ *
+ */
 public class FPDual {
-
+	
+	/**
+	 * Método Principal
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+				
 		variablesChallenge();
 
 	}
 	
+	/**
+	 * Método variables
+	 *  
+	 */
 	private static void variablesChallenge(){
 		
 		// Creamos los empleados
@@ -19,18 +33,18 @@ public class FPDual {
 		Developer emp3 = new Developer("Ana");
 		Developer emp4 = new Developer("Javier");
 		
-		// Nombre Empresa
-		System.out.println("Nombre empresa: " + emp1.getEmpresa());
-		
 		// Mostrar el número total de empleados
-		System.out.println("El número de empleados: " + emp1.getNumTotalDesarrolladores());
-		System.out.println("El número de empleados: " + emp4.getNumTotalDesarrolladores());
+		System.out.println("El número de empleados: " + Developer.getNumTotalDesarrolladores());
+		System.out.println("El número de empleados: " + Developer.getNumTotalDesarrolladores());
 
 		// Mostrar el numero de desarrolladores
 		System.out.println("El numero de empleado de Juan es: " + emp1.getIdEmpleado());
 		System.out.println("El numero de empleado de Maria es: " + emp2.getIdEmpleado());
 		System.out.println("El numero de empleado de Ana es: " + emp3.getIdEmpleado());
 		System.out.println("El numero de empleado de Javier es: " + emp4.getIdEmpleado());		
+		
+		// Mostrar el nombre de la empresa
+		System.out.println("La empresa es: " + Developer.getEmpresa());
 		
 		// Cambiar el número de vacaciones del segundo empleado
 		emp2.setDiasVacaciones(emp2.getDiasVacaciones()-2);
@@ -41,6 +55,11 @@ public class FPDual {
 		System.out.println("El número de vacaciones para Ana son: " + emp3.getDiasVacaciones());
 		System.out.println("El número de vacaciones para Javier son: " + emp4.getDiasVacaciones());	
 		
+		// Cambiar el número de dias de vacaciones lanzando la excepción.
+		System.out.println("El empleado 2 tiene " + emp2.getDiasVacaciones() + " días de vacaciones.");
+		
+			// Al tener 19 días de vacaciones no se puede restar 20 días.
+			emp2.setDiasVacaciones(emp2.getDiasVacaciones()-20);
 		
 		
 	}
--- src/variables/Developer.java
+++ src/variables/Developer.java
@@ -1,26 +1,37 @@
 package variables;
 
+/**
+ * Formación Java
+ * 
+ * @author fprietoa
+ *
+ */
 public class Developer {
 	
-	// Instanciar el nombre	
+	/** Instanciar el nombre */
 	private String nombre;
 	
-	// Instanciar el nombre	de la empresa final para que no se pueda modificar
+	/** Instanciar el nombre de la empresa final para que no se pueda modificar */
 	public final static String empresa = "Everis";
 	
-	// Instanciar el los dias de vacaciones
+	/** Instanciar el los dias de vacaciones */
 	private int diasVacaciones = 21;
 	
-	// Instanciar el id de los empleados
+	/** Instanciar el id de los empleados */
 	private int idEmpleado = 0;
 	
-	// Instanciar el numero total de desarolladores
+	/** Instanciar el numero total de desarolladores */
 	private static byte numTotalDesarrolladores = 0;
 	
-	// Constructor con el nombre
+	/** Constructor con el nombre */
 	public Developer(String nombre) {
+		
 		this.nombre = nombre;
+		
+		// Por cada Developer que se crea el número total de desarrolladores aumenta en uno.
 		this.numTotalDesarrolladores += 1;
+		
+		// El idEmpleado es igual al número total de desarrolladores en ese momento.
 		this.idEmpleado = numTotalDesarrolladores;
 		
 	}
@@ -30,7 +41,7 @@ public class Developer {
 	 * @return the nombre
 	 */
 	public String getNombre() {
-		return nombre;
+			return nombre;		
 	}
 
 	/**
@@ -38,7 +49,9 @@ public class Developer {
 	 * @param nombre the nombre to set
 	 */
 	public void setNombre(String nombre) {
-		this.nombre = nombre;
+		if (nombre != null)
+            throw new RuntimeException("El nombre no puede ser null");
+        this.nombre = nombre;
 	}	
 
 	/**
@@ -53,6 +66,8 @@ public class Developer {
 	 * @param numTotalDesarrolladores the numTotalDesarrolladores to set
 	 */
 	public static void setNumTotalDesarrolladores(byte numTotalDesarrolladores) {
+		if(numTotalDesarrolladores < 0) 
+			throw new RuntimeException("El numero total de desarrolladores no puede ser menor de 0");		
 		Developer.numTotalDesarrolladores = numTotalDesarrolladores;
 	}
 
@@ -69,6 +84,8 @@ public class Developer {
 	 * @param diasVacaciones the diasVacaciones to set
 	 */
 	public void setDiasVacaciones(int diasVacaciones) {
+		if(diasVacaciones < 0) 
+			throw new RuntimeException("No puedes restar mas dias de los que tiene.");	
 		this.diasVacaciones = diasVacaciones;
 	}
 
@@ -85,6 +102,8 @@ public class Developer {
 	 * @param idEmpleado the idEmpleado to set
 	 */
 	public void setIdEmpleado(int idEmpleado) {
+		if(idEmpleado < 0) 
+			throw new RuntimeException("El numero de empleado no puede ser menor de 0");
 		this.idEmpleado = idEmpleado;
 	}
 
@@ -99,10 +118,12 @@ public class Developer {
 	/**
 	 * Método para modificar el número de vacaciones
 	 * @return the empresa
-	 */
+	 
 	public void numVacaciones(int diasMenos) {
+		if(diasMenos > diasVacaciones) 
+			throw new RuntimeException("No puedes restar mas dias de vacaciones de las que tiene");
 		this.diasVacaciones -= diasMenos;
 	}
-	
+	*/
 
 }
